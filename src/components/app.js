d240b46a0cf05e392917acb778019c99d9ae447f
VonD <paul.vonderscher@gmail.com>
2021-01-09 20:09:59 +0100

    adding styling

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { colors, fontWeights } from 'config/styles';
 
 export default function App({ store }) {
 	return (
 		<Provider store={store}>
-			<h1>Welcome to Planity !</h1>
+			<h1 css={styles.title}>Welcome to Planity !</h1>
 		</Provider>
 	);
 }
+
+const styles = {
+	title: {
+		'margin': 10,
+		'color': colors.green500,
+		'fontWeight': fontWeights.bold,
+		'@media (min-width: 500px)': {
+			color: 'red'
+		}
+	}
+};
