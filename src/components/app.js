88a53fcafa488d8f2dee098bc84d5a833bbc8dd3
VonD <paul.vonderscher@gmail.com>
2021-01-10 19:02:37 +0100

    adding global styles

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import { Global } from '@emotion/react';
 import { colors, fontWeights } from 'config/styles';
 
 export default function App({ store }) {
 	return (
 		<Provider store={store}>
-			<h1 css={styles.title}>Welcome to Planity !</h1>
+			<div>
+				<Global styles={styles.global} />
+				<h1 css={styles.title}>Welcome to Planity !</h1>
+			</div>
 		</Provider>
 	);
 }
 
 const styles = {
+	global: {
+		'*, *:before, *:after': {
+			boxSizing: 'border-box'
+		},
+		'body': {
+			fontFamily: 'Open Sans, sans-serif'
+		}
+	},
 	title: {
 		'margin': 10,
 		'color': colors.green500,
